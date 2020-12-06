0e8b8fa0f046793bdad97d24c80d98d2d059549e
XRabell13 <XRabell13@rambler.ru>
2020-12-06 13:10:21 +0300

    Добавлена лабораторная работа №14 и ответы на вопросы по лабораторной работе №15

---
+++ 2kLabsSharp_14/CodeFile1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+interface IMovable// его методы обязательно должны быть реализованы в классах
+{
+
+
+    // методы
+    void Move(int speed);                // движение
+
+
+    // свойства
+    string NameOrganization { get; set; }   // название транспорта
+    string Marka { get; set; }  // марка транспорта
+    string Number { get; set; } // номер транспорта
+    int Speed { get; set; }
+
+
+}
+
+// то, что может быть, а может и не быть
+[Serializable]
+public abstract class Transport//общий, базовый класс. Абстрактный. Все его методы МОГУТ БЫТЬ реализованы 
+{
+
+    int MaxSpeed { get; set; }
+    int MinSpeed { get; set; }
+
+    int WayKm { get; set; }
+
+
+    public int MoveInWay(int wayKm, int speed) => wayKm / speed;//реализован уже и можно просто использовать
+
+    public abstract void MaxSpeeds(int speed);//нужно реализовать
+
+}
+
+[Serializable]
+public class Car : Transport, IMovable
+{
+
+    public string NameOrganization { get; set; }   // название транспорта
+    public string Marka { get; set; }  // марка транспорта
+    public string Number { get; set; } // номер транспорта
+
+    [NonSerialized]//несериализуемое поле
+    public readonly string ID;
+    int maxSpeed { get; set; }
+    int minSpeed { get { return minSpeed; } set { minSpeed = 1; } }//1, чтобы при делении на 0 не было ошибки
+
+    int wayKm { get { return wayKm; } set { if (value > 0) wayKm = value; else wayKm = 1; } }//сколько проехал км
+
+    public int Speed { get; set; }
+
+    public void Move(int speed)
+    {
+        Speed = speed;
+        Console.WriteLine($"Скорость установлена для Car равная: {Speed}km/h");
+    }
+
+    public override void MaxSpeeds(int speed)
+    {
+        if (speed > 0 && speed < 190) Speed = speed;
+
+        Console.WriteLine($"MaxСкорость установлена для Car равная: {Speed}km/h");
+    }
+
+    public Car(string n, string m, string num)
+    {
+        NameOrganization = n;  // название транспорта
+        Marka = m;  // марка транспорта
+        Number = num; // номер транспорта
+        MaxSpeeds(150);
+        ID = Convert.ToString(Number.GetHashCode() + NameOrganization.GetHashCode());
+    }
+
+    public Car()
+    {
+        NameOrganization = "OrganizationForCars";  // название транспорта
+        Marka = "Other";  // марка транспорта
+        Number = "1111111"; // номер транспорта
+        MaxSpeeds(150);
+       // ID = Convert.ToString(Number.GetHashCode() + NameOrganization.GetHashCode());
+    }
+
+ 
+    public sealed class Engine //двигатель
+    {
+        string Type { get; set; }
+        string Power { get; set; }
+
+        public Engine()
+        {
+            Type = "Goods";
+            Power = "240 л.с.";
+        }
+
+    }
+
+
+
+    //переопределение всех методов унаследованных от object
+    public override int GetHashCode()
+    {
+        // 269 или 47 простые
+        int hash = 269;
+        hash = string.IsNullOrEmpty(Number) ? 0 : Number.GetHashCode();
+        hash = (hash * 47) + NameOrganization.GetHashCode();
+        return hash;
+    }
+
+    public virtual Boolean Equals(Car obj)
+    {
+        // Сравниваемый объект не может быть равным null
+        if (obj == null) return false;
+        // Объекты разных типов не могут быть равны
+        //  if (this.GetType() != obj.GetType()) return false;
+        if (this.ID != obj.ID) return false;//сравниваем по ID, ибо если ID разные, то и поля в чем-то различаются
+
+        return true;
+    }
+
+    public new Type GetType()
+    {
+        return typeof(Car);
+    }
+
+    public override string ToString()
+    {
+        return "Класс: Car\nНаименование фирмы: " + NameOrganization + "\nМарка:" + Marka + "\nНомер:" + Number + "\nID:" + ID + "\nMaxSpeed:" +
+             Speed + "\nМетоды: Move, MaxSpeed\nПереопределения: GetType(),GetHashCode(), Finallize()" +
+             "toString(), Eguals()";
+    }
+
+}
---
+++ 2kLabsSharp_14/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.IO;
+using System.IO.Compression;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Xml.Linq;
+using System.Xml;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("\n ============================================= ПЕРВЫЙ ПУНКТ ============================================\n");
+
+
+        Car C1 = new Car("Cars", "Opel", "123456");
+          Car C2 = new Car("Cars","Zaphire","154456");
+          Car C3 = new Car("Org","Astra","178080");
+          Car C4 = new Car("Ord","Bently","789523");
+
+        Console.WriteLine("\n ============================================= Binary ============================================\n");
+
+        Console.WriteLine($"\n\nОбъект С1 до сериализации: \n{C1}");
+      
+        BinaryFormatter formatter = new BinaryFormatter();
+        // получаем поток, куда будем записывать сериализованный объект 
+        using (FileStream fs = new FileStream(@"C:\Users\hp\source\repos\2kLabsSharp_14\C1New.txt", FileMode.OpenOrCreate))
+        {
+            formatter.Serialize(fs, C1);
+
+        }
+        // десериализация 
+        using (FileStream fs = new FileStream(@"C:\Users\hp\source\repos\2kLabsSharp_14\C1New.txt", FileMode.OpenOrCreate))
+        {
+            Car DesC1 = (Car)formatter.Deserialize(fs);
+            Console.WriteLine($"\n\nОбъект после десериализации из Binary: \n{DesC1}");
+
+        }
+        Console.WriteLine("\n ============================================= Soap ============================================\n");
+     
+        Console.WriteLine($"\n\nОбъект С2 до сериализации: \n{C2}");
+        SoapFormatter soapFormatter = new SoapFormatter();
+        using (Stream fStream = new FileStream(@"C:\Users\hp\source\repos\2kLabsSharp_14\C2.txt", FileMode.OpenOrCreate)) 
+        {
+          
+            soapFormatter.Serialize(fStream,C2);
+          
+        }
+        using (Stream fStream = new FileStream(@"C:\Users\hp\source\repos\2kLabsSharp_14\C2.txt", FileMode.OpenOrCreate))
+        {
+
+          Car DesC2 =  (Car)soapFormatter.Deserialize(fStream);
+          Console.WriteLine($"\n\nОбъект после десериализации из Soap: \n{DesC2}");
+
+        }
+
+        Console.WriteLine("\n ============================================= Json ============================================\n");
+      
+        Console.WriteLine($"\n\nОбъект С3 до сериализации: \n{C4}");
+        DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(Car));
+        using (FileStream fs = new FileStream(@"C:\Users\hp\source\repos\2kLabsSharp_14\car4.json", FileMode.OpenOrCreate))
+        { 
+            jsonFormatter.WriteObject(fs, C4); 
+        }
+        using (FileStream fs = new FileStream(@"C:\Users\hp\source\repos\2kLabsSharp_14\car4.json", FileMode.OpenOrCreate))
+        {
+           Car DesC4 = (Car)jsonFormatter.ReadObject(fs);
+            Console.WriteLine($"\n\nОбъект после десериализации из Json: \n{DesC4}");
+
+        }
+
+        Console.WriteLine("\n ============================================= XML ============================================\n");
+
+        Console.WriteLine($"\n\nОбъект С3 до сериализации: \n{C3}");
+        XmlSerializer CarSerXML = new XmlSerializer(typeof(Car));
+
+        // получаем поток, куда будем записывать сериализованный объект
+        using (FileStream fs = new FileStream(@"C:\Users\hp\source\repos\2kLabsSharp_14\cars3.xml", FileMode.OpenOrCreate)) 
+        {
+            CarSerXML.Serialize(fs, C3); 
+        }
+        // десериализация
+        using (FileStream fs = new FileStream(@"C:\Users\hp\source\repos\2kLabsSharp_14\cars3.xml", FileMode.OpenOrCreate))
+        { 
+            Car DesC3 = CarSerXML.Deserialize(fs) as Car;
+            Console.WriteLine($"\n\nОбъект после десериализации из XML: \n{DesC3}");
+
+        }
+
+        Console.WriteLine("\n ============================================= ВТОРОЙ ПУНКТ ============================================\n");
+
+        XmlSerializer CarsXML = new XmlSerializer(typeof(Car[]));
+        Console.WriteLine($"\n\nМассив до сериализации: \n");
+
+        Car[] Cars = new Car[] { C1, C2, C3, C4 };
+        for (int i = 0; i < Cars.Length; i++)
+            Console.WriteLine($"\n{Cars[i]}");
+
+        using (FileStream fs = new FileStream(@"C:\Users\hp\source\repos\2kLabsSharp_14\carsXMLNew.xml", FileMode.OpenOrCreate))
+        {
+            CarsXML.Serialize(fs, Cars);
+        }
+        // десериализация
+        using (FileStream fs = new FileStream(@"C:\Users\hp\source\repos\2kLabsSharp_14\carsXMLNew.xml", FileMode.OpenOrCreate))
+        {
+            Car[] CarsX = CarsXML.Deserialize(fs) as Car[];
+            Console.WriteLine($"\n\nМассив после десериализации из XML: \n");
+            for (int i = 0; i < CarsX.Length; i++)
+                Console.WriteLine($"\n{CarsX[i]}");
+
+        }
+
+        Console.WriteLine("\n ============================================= ТРЕТИЙ ПУНКТ ============================================\n");
+
+        XmlDocument doc = new XmlDocument();
+        doc.Load(@"C:\Users\hp\source\repos\2kLabsSharp_14\carsXML.xml");
+        XmlNode root = doc.DocumentElement;
+ 
+        XmlNodeList nodeList;
+   
+        nodeList = root.SelectNodes("descendant::Car[Marka='Zaphire']");
+        Console.WriteLine("Информация из узлов с маркой машины Zaphire: ");
+
+        //Change the price on the books.
+        foreach (XmlNode nd in nodeList)
+        {
+            Console.WriteLine($"\nOrg: {nd.FirstChild.InnerText}\nMarka: {nd.FirstChild.NextSibling.InnerText}\nNumber: {nd.FirstChild.NextSibling.NextSibling.InnerText}\nSpeed: {nd.LastChild.InnerText}");
+
+        }
+        XmlNode nod = root.SelectSingleNode("descendant::Car[Speed>140]");
+        Console.WriteLine("\nИнформация из первого узла, значение скорости которого больше 140: ");
+
+        Console.WriteLine($"\nOrg: {nod.FirstChild.InnerText}\nMarka: {nod.FirstChild.NextSibling.InnerText}\nNumber: {nod.FirstChild.NextSibling.NextSibling.InnerText}\nSpeed: {nod.LastChild.InnerText}");
+
+
+
+        Console.WriteLine("\n ============================================= ЧЕТВЕРТЫЙ ПУНКТ ============================================\n");
+        
+        XmlDocument newdoc = new XmlDocument();
+        newdoc.Load(@"C:\Users\hp\source\repos\2kLabsSharp_14\P4ModCars.xml");
+
+        XmlNodeList nodeListNew;
+        XmlNode rootNew = newdoc.DocumentElement;
+
+        nodeListNew = rootNew.SelectNodes("descendant::Car[Speed>220]");
+
+        foreach (XmlNode nd in nodeListNew)
+        {
+           nd.FirstChild.InnerText = "NewCar2020";
+            nd.FirstChild.NextSibling.InnerText = "Tesla";
+        }
+
+        Console.WriteLine("\n\nНовосозданный документ по выборке на скорость и с заменой значений узлов: ");
+        newdoc.Save(Console.Out);
+
+        Console.WriteLine("\n\nЗначения узлов, в которых Number>800: ");
+
+        XmlNode nodNew = rootNew.SelectSingleNode("descendant::Car[Marka = 'Tesla']");
+        nodeListNew = rootNew.SelectNodes("descendant::Car[Number>800]");
+        foreach (XmlNode nd in nodeListNew)
+        {
+            Console.WriteLine($"\nOrg: {nd.FirstChild.InnerText}\nMarka: {nd.FirstChild.NextSibling.InnerText}\nNumber: {nd.FirstChild.NextSibling.NextSibling.InnerText}\nSpeed: {nd.LastChild.InnerText}");
+           
+        }
+        Console.WriteLine("\n\nЗначение первого узоа, в котором Marka='Tesla': ");
+
+        Console.WriteLine($"\nOrg: {nodNew.FirstChild.InnerText}\nMarka: {nodNew.FirstChild.NextSibling.InnerText}\nNumber: {nodNew.FirstChild.NextSibling.NextSibling.InnerText}\nSpeed: {nodNew.LastChild.InnerText}");
+
+
+    }
+}
+
+    
+
